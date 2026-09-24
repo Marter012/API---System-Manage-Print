4e3a53b0c3ed53901bd69214b96fea32712b4e7f
Mauricio Gomez <mauricio.gomez.15@hotmail.com>
2026-09-23 21:18:17 -0300

    Add vercel url

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://system-manage-print-hun9ywyup-marter012s-projects.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
